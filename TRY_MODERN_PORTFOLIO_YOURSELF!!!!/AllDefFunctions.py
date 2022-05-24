afcb20c2275bf7d930d58839f64a062ff846f209
MathewMill <maciek678945@gmail.com>
2022-05-24 09:30:50 +0200

    freq_selection_356vs252

--- TRY_MODERN_PORTFOLIO_YOURSELF!!!!/AllDefFunctions.py
+++ TRY_MODERN_PORTFOLIO_YOURSELF!!!!/AllDefFunctions.py
@@ -98,6 +98,7 @@ def find_first_date_from_choesen_stosks(start, end, tickers):
     print(f'First aveliable date to comapre stocks is {first_date}. This stock is {first_date_ticker}.')
     print('---------------------------------------------------')
     print('If this is to short period, You can always unmark stock with the lowest start date aveliable')
+    print('Remember that if you want to delete ticker from analysis u must shift+enter all cells again')
     
     return first_date
 
@@ -144,7 +145,123 @@ def check_end_date(end):
 
 # In[8]:
 
+# Function that check if any selected ticker is cryptocurrency
+def check_quote_type(tickers, start, end):
 
+    qType = pd.DataFrame(index=tickers)
+    
+    # Loop by all selected tickers and get ticker quota type from Yahoo
+    for ticker in tickers:
+        qType[ticker] = pdr.get_quote_yahoo(ticker, start = start, end = end)['quoteType'].iloc[0]
+
+    # Store all unique quota types in variable
+    unique_types = qType.iloc[0].unique()
+    for i in range(len(unique_types)):
+        
+        # If any of assets is crypto then set freq = 'choese freq', which means that user must decide if he/she want to work on
+        # full year range = 365 or business days year range = 252
+        if unique_types[i] == 'CRYPTOCURRENCY':
+
+            freq = 'choese freq'
+            break
+
+        # If there is no crypto just set freq to busieness days only
+        else:
+           
+            freq = 'business days'
+            
+    if freq == 'choese freq':
+        print('It looks like some of your assets are cryptocurrencies')
+        print('Crypto markets are listed every single day in addition to traditional stocks')
+
+    else:    
+        print('It look like that all of your assets come from the traditional market, which is quoted every business day')
+                        
+    return freq, qType
+
+# I created this function, becouse I noticed that Yahoo Finance has some missing dates for traditional Markets likie equity or commodity
+# I usually hapens, when selecting end date that is very close to current date
+# To avoid that issue I could just use my function that insert missing rows, but it would consume to much time, so
+# I decided that I can just set crypto asset (if any) at first place, which usually work well and
+# Thanks that I could just handle missing dates using fillna(method='bfill'), which copy previous value instead of Naan
+
+# This function will detect if any asset is crypto and set it to first place
+def sort_tickers(qType, tickers):
+    
+    # Create df with index as ticker name and values as ticker type
+    tic_type = pd.DataFrame(index=qType.columns, data=qType.values[0])
+    
+    # Loop by tickers type to find if any tickers are crypto
+    for i in range(len(tic_type)):
+        
+        if tic_type.iloc[i].values == 'CRYPTOCURRENCY':
+            
+            # If that is True, then add crypto ticker again at FIRST place
+            tickers.insert(0,tic_type.index[i])
+            # Then remove duplicates
+            tickers = list(dict.fromkeys(tickers))
+            break
+        else:
+            pass
+    
+    # Return sorted tickers    
+    return tickers, tic_type
+
+# Function that display checkbox if freq = 'choese freq'
+def select_freq(full_DR, busieness_DR, freq):
+    
+    if freq == 'choese freq':
+        
+        print('Now you will decide if you want to work on full date range or only business days:')
+        display(full_DR, busieness_DR)
+
+    # if freq =! 'choese freq' just pass variables
+    else:
+        pass
+        
+    return full_DR, busieness_DR
+
+
+# Function that read checkbox results, gives comment and return final choice
+def choese_freq(full_DR, busieness_DR, freq):
+    
+    if freq == 'choese freq':
+
+        # All aveliable tickers
+        freqs = [full_DR, busieness_DR]
+
+        # Tickers that user selectes
+        choesen_freq = []
+
+        # Add tickers to choesen ticker by loop
+        for freq in freqs:
+
+            if freq.get_interact_value() == True:
+                freq_name = freq.description
+                choesen_freq.append(freq_name)
+
+            else:
+                pass
+
+        # Condition: You must select at least 1 freq
+        if len(choesen_freq) <1:
+            print('You must select at least 1 frequency to continue')
+            
+        elif len(choesen_freq) > 1:
+            print('You can NOT select more than 1 frequency at a time')
+
+        # If condiction is fulfilled then print choesen freq
+        else:
+            print(f'Your choice: {choesen_freq}')
+            
+    else:
+        choesen_freq = ['252']
+        
+
+    return choesen_freq
+
+
+# This class is set of function for users who selected 365 days freq
 class PasteMissingRows:
     
     def __init__(self, ticker, start_date, end_date):
@@ -231,7 +348,7 @@ class PasteMissingRows:
     
         return df
     
-    
+# Final function that create dataset with 365 freq
 def createPrepDataFrame(tickers, start, end, PasteMissingRows):    
     
     data = pd.DataFrame()
@@ -249,6 +366,96 @@ def createPrepDataFrame(tickers, start, end, PasteMissingRows):
         data[ticker] = dt
     
     return data
+
+
+# This 2 functions below is for users who picked 252 days freq or selected only traditional stocks (no crypto in wallet)
+
+# Cutting funcion in perpouse to create proper dataset
+def cut_weekends(start,end, data):
+    
+    # Create date range with proper busieness dates only
+    prop_dRange = pd.bdate_range(start, end)
+    
+    # For some reason yahoo finance download one extra day afrer end date for crypt
+    # So I must cut it
+    data = data[:-1]
+    
+    # loop by any row in data
+    for i in range(len(data)):
+        
+        # If weekday of specific date is equal 5 it means that this is saturday
+        if data.index[i].weekday() == 5:
+            
+            # Drop saturday
+            data.drop(data.index[i], inplace=True)
+            
+            # Now Sunday is in posiotion of i, becouse we already cut Saturday, so I just need to make one more drop here
+            data.drop(data.index[i], inplace=True)
+            
+        else:
+            # If len of busines day index created by hand is equal new cuted data, then brak loop
+            if len(prop_dRange) == len(data):
+                break
+        
+    return data
+
+# Final function that create df for 252 freq
+def create_data_without_weekends(tickers, start, end, cut_weekends):
+    
+    data = pd.DataFrame()
+
+    # loop by tickers
+    for ticker in tickers:
+
+        # First we must check if asset is crypt
+        qType = pdr.get_quote_yahoo(ticker, start = start, end = end)['quoteType'].iloc[0]
+
+        if qType == 'CRYPTOCURRENCY':
+
+            # download data
+            dt = pdr.get_data_yahoo([ticker], start = start, end = end)['Adj Close']
+            # Cut weekends
+            dt = cut_weekends(start, end, dt)
+
+
+        else:
+            # If no, just download data
+            dt = pdr.get_data_yahoo([ticker], start = start, end = end)['Adj Close']
+
+
+        # Add column named as ticker with values from function
+        data[ticker] = dt
+        
+    # In cease, there is some missing dates in Yahoo finance
+    data.fillna(method='bfill', inplace=True)
+    
+    return data
+
+# Compare all function to create datasets with choesen freq (252 or 365)
+def createDF_with_choesen_ferq(choesen_freq, tickers, start, end, createPrepDataFrame, PasteMissingRows, create_data_without_weekends, cut_weekends):
+   
+    if choesen_freq[0] == '365':
+
+        # Create 365days per year index and prepare data by functio
+        data = createPrepDataFrame(tickers, start, end, PasteMissingRows)
+        n = 365
+
+    elif choesen_freq[0] == '252':
+
+        # Create 252 days per year index and prepare data
+        # Even if user did not choese crypto for his portfolio it will fill missinig values
+        data = create_data_without_weekends(tickers, start, end, cut_weekends)
+        n = 252
+
+
+    else:
+        print('You didn\'t selected frequency!')
+        
+    # It returns dataset and n, which is period that will be helpful in calculating annual vol and rets for Markowitz model
+    return data, n
+
+
+
 
 
 # # Markowitz Model functions
@@ -275,26 +482,26 @@ def gen_weights(df):
     weights = np.random.random(N)
     return weights / np.sum(weights)
 
-def calculate_returns(weights, log_rets):
+def calculate_returns(weights, log_rets, n):
     
     # Calculate logaritmic returns for given weights
-    returns = np.sum(log_rets.mean()*weights) * 365
+    returns = np.sum(log_rets.mean()*weights) * n
     return returns
 
-def calculate_volatility(weights, log_rets):
+def calculate_volatility(weights, log_rets, n):
 
     # We uce covariance instead of log_rets
     log_rets_cov = log_rets.cov()
 
     # Calc annual covariance for given weight
-    annualized_cov = np.dot(log_rets_cov*365, weights)
+    annualized_cov = np.dot(log_rets_cov*n, weights)
     # Use covariance to calculate volotality and std
     vol = np.dot(weights.transpose(),annualized_cov)
     volatility = np.sqrt(vol)
 
     return volatility
 
-def symulatePortfoliosWeights(df, iterations, calculate_N, calculate_log_rets, gen_weights, calculate_returns, calculate_volatility):        
+def symulatePortfoliosWeights(df, iterations,n, calculate_N, calculate_log_rets, gen_weights, calculate_returns, calculate_volatility):        
 
     # Chceck how many stock been choesen
     N = calculate_N(df)
@@ -313,10 +520,10 @@ def symulatePortfoliosWeights(df, iterations, calculate_N, calculate_log_rets, g
         weights = gen_weights(df)
         portfolio_weights.append(weights)
 
-        sim_returns = calculate_returns(weights, log_rets)
+        sim_returns = calculate_returns(weights, log_rets, n)
         portfolio_returns.append(sim_returns)
 
-        sim_vol = calculate_volatility(weights, log_rets)
+        sim_vol = calculate_volatility(weights, log_rets, n)
         portfolio_vol.append(sim_vol)
 
     # Create DF wieth all calculated features
@@ -645,25 +852,32 @@ def choese_rebalancing_frquency(quaterly, yearly):
         
     return choesen_frq
 
-def create_dates_for_rebalancing(df, frequency):
+def create_dates_for_rebalancing(df, n, frequency):
     
     start = df.index[0]
     
     if frequency =='quaterly':
 
         # How many quaters in period
-        quaters = (len(df)/365)*4
+        quaters = (len(df)/n)*4
         # Number of months i one quater multiplyed by quaters
         quantity = np.floor(quaters*3).astype(int)
         rebalancing_dates = []
         for i in range(3, quantity, 3):
+            
             rebalancing_dates.append(start + relativedelta(months=i))
             
-        print(f'In this period you could make {np.floor(quantity/3).astype(int)} rebalancing')
+            
+        if rebalancing_dates[-1] > df.index[-1]:
+            rebalancing_dates = rebalancing_dates[:-1]
+            
+        reb_quant = len(rebalancing_dates)
+            
+        print(f'In this period you could make {reb_quant} rebalancing')
 
     elif frequency =='yearly':
 
-        years = len(df)/365
+        years = len(df)/n
         quantity = np.floor(years+1).astype(int) # I add one becouse we start after first year in loop
         rebalancing_dates = []
 
@@ -672,7 +886,12 @@ def create_dates_for_rebalancing(df, frequency):
             # Add i year from first transaction
             rebalancing_dates.append(start + relativedelta(years=i))
             
-        print(f'In this period you could make {quantity-1} rebalancing')
+        if rebalancing_dates[-1] > df.index[-1]:
+            rebalancing_dates = rebalancing_dates[:-1]
+            
+        reb_quant = len(rebalancing_dates)
+            
+        print(f'In this period you could make {reb_quant} rebalancing')
 
     else:
         print('You must select period of frequency')
@@ -681,6 +900,32 @@ def create_dates_for_rebalancing(df, frequency):
     rebalancing_dates = np.array(rebalancing_dates)
     
     return rebalancing_dates
+
+# In case some of selected automaticly dates is at weekends (for 252 freq only)
+def repair_rebalancing_dates(rebalancing_dates, n):
+    
+    if n == 252:
+        reb_dates = pd.DataFrame(rebalancing_dates)
+        prep_reb_dates = []
+        for i in range(len(reb_dates)):
+
+            if reb_dates.iloc[i,0].weekday() == 5:
+                new_date = reb_dates.iloc[i,0] + dt.timedelta(days=-1)
+                prep_reb_dates.append(new_date)
+
+            elif reb_dates.iloc[i,0].weekday() == 6:
+                new_date = reb_dates.iloc[i,0] + dt.timedelta(days=-2)
+                prep_reb_dates.append(new_date)
+
+            else:
+                new_date = reb_dates.iloc[i,0]
+                prep_reb_dates.append(new_date)
+                
+    else:
+        prep_reb_dates = rebalancing_dates
+        
+    return prep_reb_dates
+
 
 def create_new_wallet_with_rebalanceing(df, weights, rebalancing_dates, buyAmt, cum_returns, proportions):
     
@@ -767,20 +1012,20 @@ def create_table_with_all_portfolios(cum_returns, portfolio_value):
     return all_portfolios
 
 
-def dropdown_summary(all_portfolios):
+def dropdown_summary(all_portfolios, n):
     
     # Create another table with dropdowns
     dropdowns = pd.DataFrame(round(all_portfolios.pct_change(30).dropna().min()*100,2)) # Montlhy
     dropdowns = dropdowns.rename(columns={0:'Month'})
     dropdowns['Quater'] = pd.DataFrame(round(all_portfolios.pct_change(90).dropna().min()*100,2)) # Quater
-    dropdowns['Year'] = pd.DataFrame(round(all_portfolios.pct_change(365).dropna().min()*100,2))
+    dropdowns['Year'] = pd.DataFrame(round(all_portfolios.pct_change(n).dropna().min()*100,2))
     dropdowns[['Month', 'Quater', 'Year']] = dropdowns[['Month', 'Quater', 'Year']].astype(str) + '%'
     
     print('MAx dropdowns summary:')
     
     return dropdowns
 
-def returns_summary(all_portfolios, investmentAmt):
+def returns_summary(all_portfolios, investmentAmt, n):
     
     # create DF with all portfolios
     returns = pd.DataFrame(index=all_portfolios.columns)
@@ -800,9 +1045,9 @@ def returns_summary(all_portfolios, investmentAmt):
     returns['Quaterly'] = [np.round(equalQ,2), np.round(YourQ,2), np.round(BalQ,2)]
 
     # Calculate Yearly returns for 3 portfolios
-    equalY = ((all_portfolios.iloc[-1,0]/investmentAmt)**(1/len(all_portfolios)*365) - 1)*100
-    YourY = ((all_portfolios.iloc[-1,1]/investmentAmt)**(1/len(all_portfolios)*365) - 1)*100
-    BalY = ((all_portfolios.iloc[-1,2]/investmentAmt)**(1/len(all_portfolios)*365) - 1)*100
+    equalY = ((all_portfolios.iloc[-1,0]/investmentAmt)**(1/len(all_portfolios)*n) - 1)*100
+    YourY = ((all_portfolios.iloc[-1,1]/investmentAmt)**(1/len(all_portfolios)*n) - 1)*100
+    BalY = ((all_portfolios.iloc[-1,2]/investmentAmt)**(1/len(all_portfolios)*n) - 1)*100
     # append to df
     returns['Yearly'] = [np.round(equalY,2), np.round(YourY,2), np.round(BalY,2)]
     
